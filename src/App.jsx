93348df1d558361ce223c46e53fa620fe319e014
eriberiii <ushangerica@gmail.com>
2026-09-03 21:50:19 +0100

    adding infinite scroll functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 function App(){
+  const[movies, setMovies] = useState([]);
   return(
+    <>
     <input type='text' className="searchbar" placeholder="Search"/>
     <nav className="navbar">
       <ul>
@@ -8,5 +11,44 @@ function App(){
         <li>Series</li>
       </ul>
     </nav>
+    <div className="moviebar">
+      {movies.map((movie) =>(
+        <div key={movie.id} className="movie-card">
+          <img src={movie.Poster}/>
+          <p>{movie.Title}</p>
+          </div>
+      ))}
+
+    </div>
+    </>
   )
+
 }
+
+export default App;
+
+
+
+const [user, setUser] = useState(["name": "Erica", "age": 21, "city": "los ang"])
+
+const handleChange = (e) =>{
+  const{name, value} = e.target;
+
+  setUser((prevUser) => ({...prevUser, [name]: value}))
+}
+const additem= () => {
+
+}
+
+const Counter = () =>{
+  const 
+  useEffect(() => {
+    document.title = `the title is ${counter}`
+  }, [counter])
+}
+
+return(
+  <>
+  <button onClick= {Counter}></button>
+  </>
+)
