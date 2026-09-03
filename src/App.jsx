0836be63e1d694518b84d6265993ddd79bfb162f
eriberiii <ushangerica@gmail.com>
2026-09-03 14:31:35 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App(){
+  return(
+    <input type='text' className="searchbar" placeholder="Search"/>
+    <nav className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>Movies</li>
+        <li>Series</li>
+      </ul>
+    </nav>
+  )
+}
